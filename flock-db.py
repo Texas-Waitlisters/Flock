e8db21a18dc15e2add4a657dc1e12eec6e9bcc50
Alex <alexrbellon@gmail.com>
2018-07-14 14:43:54 -0500

    Started python script to interact with database

---
+++ flock-db.py
@@ -0,0 +1,20 @@
+def searchByText(text):
+    #expedia's nlp
+
+def searchByCoordinates(lat, lon):
+    #find location by coordinates
+    
+def getUserData(acct_id):
+    #all cols from Users
+
+def login(username, password):
+    #log in to app
+
+def getRentalsAtLocation(loc_id):
+    #return active rentals
+
+def approveRental(rental_id):
+    #approve a rental 
+
+def requestRental(acct_id, loc_id):
+    #create a rental entry in the database
